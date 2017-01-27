ca649b35d49ce5d4920a78cec1d5c140645e639b
Caue <nncl@live.com>
2017-01-27 15:44:19 -0200

    Getting closer to API stuff

--- server.js
+++ server.js
@@ -10,19 +10,29 @@ const app = require('express')()
     , io = require('socket.io')(http)
     , socket = require('socket.io-client')('http://localhost:8080');
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+app.options('*', function (req, res) {
+    res.send(200);
+});
+
 app.get('/', function (req, res) {
-    io.emit('nickname', '@caue');
-    res.send('cool');
+    res.json({success: true, message: 'Message has been sent successfully'});
 });
 
 io.on('connect', function (socket) {
     console.log('connect server');
 
-    socket.on('nickname', function(msg){
+    socket.on('nickname', function (msg) {
         io.emit('nickname', msg);
     });
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('user disconnect');
     });
 });
